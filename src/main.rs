69052d70424a03651cd511c70d48323a8e90f4e4
Will Hawkins <whh8b@virginia.edu>
2017-05-13 01:34:48 +0000

    Refactor thanks to @commandline suggestions.

--- src/main.rs
+++ src/main.rs
@@ -16,56 +16,43 @@ struct SlackResponse {
 	text : String
 }
 
-fn main() {
-	fn respond(req: &mut Request) -> IronResult<Response> {
-		let ps = req.get_ref::<Params>().unwrap();
-		let man_param: String;
-		let mut had_param: bool = false;
-		let response_body : String;
+fn respond(req: &mut Request) -> IronResult<Response> {
+	let ps = req.get_ref::<Params>().unwrap();
+	let response_body : String;
 
-		match ps.find(&["text"]) {
-			Some(&params::Value::String(ref value)) => 
-				{
-					had_param = true;
-					man_param = value.clone().replace(";", "");
-				},
-			_ => 
-				{
-					man_param = "".to_string()
+	if let Some(&params::Value::String(ref value)) = ps.find(&["text"]) {
+		let man_param = value.clone().replace(";", "");
+		let man = Command::new("/bin/bash")
+			.arg("-c")
+			.arg(format!("man {}", man_param))
+			.output()
+			.expect("failed!");
+
+		if let Some(0) = man.status.code() {
+			println!("man() succeeded.");
+			response_body = json::encode(
+				&SlackResponse{
+					response_type: "ephemeral".to_string(),
+					text: String::from_utf8_lossy(&man.stdout).into_owned(),
 				}
-		};
-
-		println!("man_param: {}", man_param);
-
-		if had_param == true {
-			let man = Command::new("/bin/bash")
-				.arg("-c")
-				.arg(format!("man {}", man_param))
-				.output()
-				.expect("failed!");
-
-			if man.status.code().unwrap() == 0 {
-				println!("man() succeeded.");
-				response_body = json::encode(
-					&SlackResponse{
-						response_type: "ephemeral".to_string(),
-						text: String::from_utf8_lossy(&man.stdout).into_owned(),
-					}
-				).unwrap();
-			} else {
-				println!("man() failed.");
-				response_body = "".to_string();
-			}
+			).unwrap();
 		} else {
-			println!("No parameter.");
+			println!("man() failed.");
 			response_body = "".to_string();
 		}
-
-		println!("response_body: {}", response_body);
-		let mut response = Response::with((status::Ok, response_body));
-		response.headers.set(iron::headers::ContentType::json());
-		Ok(response)
+	} else {
+		println!("No parameter.");
+		response_body = "".to_string();
 	}
+
+	println!("response_body: {}", response_body);
+	let mut response = Response::with((status::Ok, response_body));
+	response.headers.set(iron::headers::ContentType::json());
+	Ok(response)
+}
+
+
+fn main() {
 	let ssl = NativeTlsServer::new("<path to pkcs12 file>","<password>").unwrap();
 	let _server = Iron::new(respond).https("0.0.0.0:5001", ssl).unwrap();
 	println!("Running ...");
